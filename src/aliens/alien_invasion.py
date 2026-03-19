bf88568c4d0f81a43ed3efd8291376fa4dc34e84
rw42 <roguewizard42@gmail.com>
2026-03-19 19:01:58 -0400

    add settings file | add game set up file

---
+++ src/aliens/alien_invasion.py
@@ -0,0 +1,38 @@
+# invasion file
+
+import sys
+import pygame
+from .settings import Settings
+
+
+class AlienInvasion():
+    """Management for game assets and behavior."""
+    
+
+    def __init__(self) -> None:
+        """Initialize game"""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("AlienInvasion")
+        
+
+    def run_game(self) -> None:
+        """Start the main game loop"""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            self.screen.fill(self.settings.bg_color)
+            pygame.display.flip()
+            # set frame rate
+            self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
+
+
---
+++ src/aliens/settings.py
@@ -0,0 +1,11 @@
+# these *are* the droids you're looking for
+
+class Settings():
+
+    def __init__(self) -> None:
+        # screen settings
+        self.screen_width: int = 1200
+        self.screen_height: int = 800
+        self.bg_color: tuple[int, int, int] = (0, 0, 0)
+
+
