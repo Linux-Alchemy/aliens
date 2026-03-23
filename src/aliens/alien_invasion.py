db770c834414ba4bab1c3faf2d0a45bc0f584425
rw42 <roguewizard42@gmail.com>
2026-03-23 19:29:51 -0400

    refactor _check_events() to manage key press events

--- src/aliens/alien_invasion.py
+++ src/aliens/alien_invasion.py
@@ -29,23 +29,30 @@ class AlienInvasion():
             # set frame rate
             self.clock.tick(60)
 
+    def _check_key_down(self, event) -> None:
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_key_up(self,event) -> None:
+        if event.key == pygame.K_RIGHT:
+           self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+           self.ship.moving_left = False
+
     def _check_events(self) -> None:
         """listening for game events"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_key_down(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
-
+                self._check_key_up(event)
+            # option to quit with 'q'
+            elif event.key == pygame.K_q:
+                sys.exit()
 
     def _update_screen(self) -> None:
         """screen update and drawing images"""
