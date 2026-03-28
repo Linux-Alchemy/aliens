496c42224356e11fff753ccac90057f50f598d2a
rw42 <roguewizard42@gmail.com>
2026-03-27 20:39:54 -0400

    add second bullet test | all tests passed

--- tests/test_bullet.py
+++ tests/test_bullet.py
@@ -11,7 +11,24 @@ def test_bullet_spawns_correct_position(pygame_game) -> None:
     expected_position = pygame_game.ship.rect.midtop
     bullet = Bullet(pygame_game)
     assert bullet.rect.midtop == expected_position
-    assert bullet.y == bullet.rect.y
+    assert bullet.y == float(bullet.rect.y)
+
+def test_bullet_movement(pygame_game) -> None:
+    """Checking that update() moves bullet upwards by bullet_speed
+
+    bullet y value should decrease as bullet moves up the screen
+
+    Args:
+        game: Instance of AlienInvasion"""
+    bullet = Bullet(pygame_game)
+    start_y = bullet.rect.y
+    speed = pygame_game.settings.bullet_speed
+    bullet.update()
+    assert bullet.rect.y == (start_y - speed)
+    assert bullet.y == float(bullet.rect.y)
+
+
+
     
 
 
